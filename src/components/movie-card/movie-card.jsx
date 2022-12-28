71d870fa33881ba62e6db81d540ebf18088cc4dd
jordanl412 <jordanlazan@gmail.com>
2022-12-28 10:18:37 -0500

    Created MovieCard

---
+++ src/components/movie-card/movie-card.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const MovieCard = ({ movie, onMovieClick }) => {
+    return (
+        <div
+            onClick={() => {
+                onMovieClick(movie);
+            }}
+        >
+            {movie.title}
+        </div>
+    );
+};
