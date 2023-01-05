09b6e8d2260a93081ad9f66b43a05e9ce0c05c3a
jordanl412 <jordanlazan@gmail.com>
2023-01-04 23:11:48 -0500

    Created LoginView
    Allows users to login to myFlix

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+import { useState } from "react";
+import PropTypes from "prop-types";
+
+export const LoginView = ({ onLoggedIn }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = (event) => {
+    //this prevents the defaut behavior of the form which is to reload the entire page
+    event.preventDefault();
+
+    const data = {
+        Username: username,
+        Password: password
+    };
+
+    fetch("https://witty-boa-tights.cyclic.app/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+        .then((response) => response.json())
+        /*{
+            return response.json()
+        })*/
+        .then((data) => {
+            console.log("Login response: ", data);
+            if (data.user) {
+                localStorage.setItem("user", JSON.stringify(data.user));
+                localStorage.setItem("token", data.token);
+                onLoggedIn(data.user, data.token);
+            } else {
+                alert("No such user");
+            }
+        })
+        .catch((e) => {
+            alert("Something went wrong");
+        });
+  }
+
+    /*const data = {
+      access: username,
+      secret: password
+    };
+
+    fetch("https://openlibrary.org/account/login.json",
+    {
+      method: "POST",
+      body: JSON.stringify(data)
+    }).then((response) => {
+      if (response.ok) {
+        onLoggedIn(username);
+      } else {
+        alert("Login failed");
+      }
+    });
+  };*/
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+          minLength="6"
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+          minLength="6"
+        />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
